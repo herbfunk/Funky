83cf540e76e6b9ad777b7398f26c13c216d7f42c
herbfunk <aaronmault@gmail.com>
2013-08-02 11:18:13 -0400

    Deleted old files

--- FunkyTrinity/Bot/Cache/Collection/ObjectCache.cs
+++ FunkyTrinity/Bot/Cache/Collection/ObjectCache.cs
@@ -19,7 +19,7 @@ namespace FunkyTrinity
 		  ///<summary>
 		  ///Contains Collections for all the cached objects being tracked.
 		  ///</summary>
-		  public partial class ObjectCache
+		  public static partial class ObjectCache
 		  {
 				///<summary>
 				///Cached Objects.
