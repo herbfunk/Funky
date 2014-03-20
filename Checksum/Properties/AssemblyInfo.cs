9e508429afb1cc5e03eec1debf981a0b125d2da8
herbfunk <aaronmault@gmail.com>
2014-03-20 19:42:52 -0400

    checksum
    Moved Project Folder out of FunkyBot.

--- Checksum/Properties/AssemblyInfo.cs
+++ Checksum/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("CheckSum")]
+[assembly: AssemblyTitle("Checksum")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyProduct("CheckSum")]
+[assembly: AssemblyProduct("Checksum")]
 [assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("3c0fae5b-c77d-4601-9f50-484000338a9f")]
+[assembly: Guid("23caea9b-0d2d-49cb-8d8c-5e14a72a118f")]
 
 // Version information for an assembly consists of the following four values:
 //
